9cb2f184606ae86df8a7a4059a320557e09bc6d9
MBerguer <martin.berguer@gmail.com>
2023-07-15 19:43:17 -0300

    removing not needed pages

--- front/src/pages/menu.js
+++ front/src/pages/menu.js
@@ -8,83 +8,23 @@ const menus = [
         id: 1,
         sub: "Home 01",
         links: "/",
-      },
-      {
-        id: 2,
-        sub: "Home 02",
-        links: "/home-02",
-      },
-      {
-        id: 3,
-        sub: "Home 03",
-        links: "/home-03",
-      },
-      {
-        id: 4,
-        sub: "Home 04",
-        links: "/home-04",
-      },
-      {
-        id: 5,
-        sub: "Home 05",
-        links: "/home-05",
-      },
-      {
-        id: 6,
-        sub: "Home 06",
-        links: "/home-06",
-      },
-      {
-        id: 7,
-        sub: "Home 07",
-        links: "/home-07",
-      },
-      {
-        id: 8,
-        sub: "Home 08 (Special)",
-        links: "/home-08",
-      },
+      }
     ],
   },
   {
     id: 2,
     name: "Explore",
     links: "#",
-    namesub: [
-      {
-        id: 1,
-        sub: "Explore 01",
-        links: "/explore-01",
-      },
-      {
-        id: 2,
-        sub: "Explore 02",
-        links: "/explore-02",
-      },
-      {
-        id: 3,
-        sub: "Explore 03",
-        links: "/explore-03",
-      },
+    namesub: [      
       {
         id: 4,
         sub: "Explore 04",
-        links: "/explore-04",
-      },
-      {
-        id: 5,
-        sub: "Live Auctions",
-        links: "/live-auctions",
-      },
-      {
-        id: 6,
-        sub: "Item Details 01",
-        links: "/item-details-01",
+        links: "/explore",
       },
       {
         id: 7,
         sub: "Item Details 02",
-        links: "/item-details-02",
+        links: "/item-details2",
       },
     ],
   },
@@ -97,12 +37,7 @@ const menus = [
         id: 1,
         sub: "Activity 01",
         links: "/activity-01",
-      },
-      {
-        id: 2,
-        sub: "Activity 02",
-        links: "/activity-02",
-      },
+      }
     ],
   },
   {
@@ -110,16 +45,6 @@ const menus = [
     name: "Community",
     links: "#",
     namesub: [
-      {
-        id: 1,
-        sub: "Blog",
-        links: "/blog",
-      },
-      {
-        id: 2,
-        sub: "Blog Details",
-        links: "/blog-details",
-      },
       {
         id: 3,
         sub: "Help Center",
@@ -132,11 +57,6 @@ const menus = [
     name: "Page",
     links: "#",
     namesub: [
-      {
-        id: 1,
-        sub: "Authors 01",
-        links: "/authors-01",
-      },
       {
         id: 2,
         sub: "Authors 02",
@@ -156,11 +76,6 @@ const menus = [
         id: 5,
         sub: "Edit Profile",
         links: "/edit-profile",
-      },
-      {
-        id: 6,
-        sub: "Ranking",
-        links: "/ranking",
       },
       {
         id: 7,
@@ -189,11 +104,6 @@ const menus = [
     name: "Contact",
     links: "/contact-01",
     namesub: [
-      {
-        id: 1,
-        sub: "Contact 1",
-        links: "/contact-01",
-      },
       {
         id: 2,
         sub: "Contact 2",
