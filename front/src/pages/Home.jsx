f8570db0a6df1d9a9280cc618360eb938c385dd8
MBerguer <martin.berguer@gmail.com>
2023-07-15 21:45:26 -0300

    adding images and modifying the home

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import HeaderHome from "../components/header/HeaderHome";
 import Footer from "../components/footer/Footer";
 import Category from "../components/layouts/home/Category";
@@ -10,6 +10,28 @@ import Create from "../components/layouts/home/Create";
 import PopularCollection from "../components/layouts/home/PopularCollection";
 
 const Home = () => {
+  useEffect(() => {
+    window.addEventListener("error", (e) => {
+      if (
+        e.message === "ResizeObserver loop limit exceeded" ||
+        e.message === "Script error."
+      ) {
+        const resizeObserverErrDiv = document.getElementById(
+          "webpack-dev-server-client-overlay-div"
+        );
+        const resizeObserverErr = document.getElementById(
+          "webpack-dev-server-client-overlay"
+        );
+        if (resizeObserverErr) {
+          resizeObserverErr.setAttribute("style", "display: none");
+        }
+        if (resizeObserverErrDiv) {
+          resizeObserverErrDiv.setAttribute("style", "display: none");
+        }
+      }
+    });
+  }, []);
+
   return (
     <div className="home">
       <HeaderHome />
